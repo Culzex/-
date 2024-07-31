b745040914687a1c55d807f6783ce6aefbc49c81
Culzex <168218968+Culzex@users.noreply.github.com>
2024-07-31 07:01:14 +0100

    Add files via upload

---
+++ Godfather/lib/search.js
@@ -0,0 +1,14 @@
+const prefix = global.prefa
+  const pushname = m.pushName || "No Name"
+
+const searchmenu=`「 *BOT* 」
+♡ۣۜۜ፝͜͜͡͡✿➤ *INFO BOT*
+  ♡ۣۜۜ፝͜͜͡͡✿➤ Name:BOT
+ ----------------------------------
+🍁 *Search Commands* 🍁
+⌬${prefix}weather
+⌬${prefix}coffee
+⌬${prefix}google
+
+ module.exports = searchmenu
+ 
---
+++ Godfather/lib/weeb.js
@@ -0,0 +1,15 @@
+const prefix = global.prefa
+  const pushname = m.pushName || "No Name"
+
+const weebmenu=`
+「 *BOT* 」
+🍁 *Weeb Commands* 🍁
+⌬${prefix}crossplay
+⌬${prefix}neko
+⌬${prefix}loli
+⌬${prefix}wallpaper
+⌬${prefix}waifu
+
+ `
+ module.exports = weebmenu
+ 
